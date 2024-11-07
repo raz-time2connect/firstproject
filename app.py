2c181390f7483195dc351e2ebfbf5a49513dd692
Raz <raz@tsook.nettime2connect.net>
2024-11-07 17:19:37 +0300

    Updated app to use Render-compatible host and port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +7,8 @@ app = Flask(__name__)
 def home():
     return "Hello, World! This is my first server."
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # שימוש בפורט מ-Render או 5000 כברירת מחדל
+    app.run(host="0.0.0.0", port=port, debug=True)
